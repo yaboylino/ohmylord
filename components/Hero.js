06c5beb2863ba526260c1b1af8e860da8727e8d5
yaboylino <lionellord@gmail.com>
2021-01-27 01:38:53 +0100

    0.0.3

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react"
-import backgroundVideo from "../public/video2.mp4"
+import backgroundVideo from "../public/video.mp4"
 
 export default function Hero() {
   const videoRef = useRef()
@@ -7,17 +7,17 @@ export default function Hero() {
   useEffect(() => {
     setTimeout(() => {
       videoRef.current.play()
-    }, 5000)
+    })
   }, [])
 
   return (
     <div className="hero">
       <div className="wrapper flex flex-col justify-center items-center h-2/3">
-        <video className="z-0" ref={videoRef} height="800" loop muted>
-          <source src={backgroundVideo} />
+        <video className="z-0" ref={videoRef} width="3000" loop muted>
+          <source src={backgroundVideo} type="video/mp4" />
         </video>
         <div className="absolute z-10 flex flex-col justify-center">
-        <h5 className="text-2xl text-white text-center uppercase mb-4">Welcome to</h5>
+          <h5 className="text-2xl text-white text-center uppercase mb-4">Welcome to</h5>
           <h1 className="md:text-8xl sm:text-6xl text-5xl text-white text-center uppercase mb-4">Oh My Lord</h1>
           <p className="md:text-xl text-sm text-white text-center uppercase">We make beatz that make you go.....</p>
         </div>
--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -1,15 +1,16 @@
-import React, {useRef, useEffect} from 'react'
-import AudioPlayer from 'react-h5-audio-player';
+import React, { useRef, useEffect } from "react"
+import AudioPlayer from "react-h5-audio-player"
 
 const MusicPlayer = () => {
-  return (
-<AudioPlayer
-    autoPlay
-    src="./audio.mp3"
-    onPlay={e => console.log("onPlay")}
-  />
-      
-  )
+  const audioRef = useRef()
+  useEffect(() => {
+    console.log(audioRef.current)
+    setTimeout(() => {
+      audioRef.current.handlePlay()
+    }, 2000)
+  }, [])
+
+  return <AudioPlayer autoPlay src="./audio.mp3" ref={audioRef} onPlay={e => console.log("onPlay")} />
 }
 
 export default MusicPlayer
